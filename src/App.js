55a01c034b50e3b9babced1bca3cf6faca03db4e
caozhengqian <12264419544@qq.com>
2019-11-11 00:06:15 +0800

    22

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import aa from './img/git.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/*<header className="App-header">*/}
+      <header >
+       <div className="text-div" >
+           <p className="test-p1">An <div  class="top"  >divdiv</div> image with a text-bottom alignment.</p>
+
+       </div>
+
       </header>
     </div>
   );
