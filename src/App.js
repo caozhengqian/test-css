9bc36dcf411d13e5debc4c98f9f12329c873f627
shenzw.ext <Fwd123456>
2019-11-15 09:24:14 +0800

    增加路由

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import aa from './img/git.png'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import BackgroundImg from './background/backgroundImg';
 
 function App() {
   return (
-    <div className="App">
-      {/*<header className="App-header">*/}
-      <header >
-       <div className="text-div" >
-           <p className="test-p1">An <div  class="top"  >divdiv</div> image with a text-bottom alignment.</p>
-
-       </div>
-
-      </header>
-    </div>
+      <Router>
+          <Switch>
+              <Route exact path="/">
+                  <BackgroundImg />
+              </Route>
+              <Route path="/background">
+                  <BackgroundImg />
+              </Route>
+          </Switch>
+      </Router>
   );
 }
+
 
 export default App;
---
+++ src/background/backgroundImg.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './backgroundImg.css'
+function BackGroundImg() {
+    return (
+        <div className="App">
+
+        </div>
+    );
+}
+
+export default BackGroundImg;
