18cde496a9237571fb23f91b6746d7ef50150777
FHQITS09 <Abcd123456>
2019-12-06 21:31:17 +0800

    增加null处理

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import {
     Route,
 } from "react-router-dom";
 import BackgroundImg from './background/backgroundImg';
+import Null from "./null/null"
 
 function App() {
   return (
       <Router>
           <Switch>
               <Route exact path="/">
-                  <BackgroundImg />
+                  <Null />
               </Route>
               <Route path="/background">
                   <BackgroundImg />
+              </Route>
+              <Route path="/null">
+                  <Null />
               </Route>
           </Switch>
       </Router>
--- src/null/null.js
+++ src/null/null.js
@@ -8,14 +8,44 @@ class Null extends React.Component {
         };
     }
     componentDidMount(){
-    const data = {a:{b:{c:{d:"dd"}}}}
-        //1、没有g，但是不会报错
-        this.setState({showData:data.a.g},function () {
-            console.info(data.a.g)
-        })
 
+        const data = {a:{b:{c:{d:"dd"}}}}
+        console.info(_.at(data,"a.g.f"));
+        //1111111111111111111111111111111111111111111111111
+         //1、没有g，但是不会报错，值为undefine，界面显示为空
+         this.setState({showData:data.a.g})//undefine
+
+        //22222222222222222222222222222222222222222222222222
         //2、TypeError: Cannot read property 'f' of undefined
         // this.setState({showData:data.a.g.f})//没有g，导致取不到f，报错
+
+        //33333333333333333333333333333333333333333333333333
+        //[undefind]
+        this.setState({showData:_.at(data,"a.g.f")},function () {
+            console.info(this.state.showData)
+        })
+
+        //44444444444444444444444444444444444444444444444444
+        let abc;//undefined
+         if(abc){
+             console.info(abc)
+             console.info("abc,true")
+         }else{
+             //走false这里
+             console.info(abc)
+             console.info("abc,false")
+         }
+
+        //555555555555555555555555555555555555555
+        let abc5;//undefined
+        if(abc5){
+            console.info(abc5)
+            console.info("abc5,true")
+        }else{
+            //走false这里
+            console.info(abc5)
+            console.info("abc5,false")
+        }
     }
 
     render() {
